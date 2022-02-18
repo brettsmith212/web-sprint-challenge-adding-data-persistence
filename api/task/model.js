78a5686e0cd99dd6025c0acd77ea8dc117fe5324
Brett Smith <brettsmith212@gmail.com>
2022-02-17 22:22:46 -0800

    get and post tasks done

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,46 @@
 // build your `Task` model here
+const db = require("../../data/dbConfig");
+
+async function getTasks() {
+  const tasks = await db("tasks as t")
+    .join("projects as p", "p.project_id", "t.project_id")
+    .select(
+      "t.task_id",
+      "t.task_description",
+      "t.task_notes",
+      "t.task_completed",
+      "p.project_name",
+      "p.project_description"
+    );
+  return tasks.map((task) => {
+    return {
+      task_id: task.task_id,
+      task_description: task.task_description,
+      task_notes: task.task_notes,
+      task_completed: Boolean(task.task_completed),
+      project_name: task.project_name,
+      project_description: task.project_description,
+    };
+  });
+}
+
+async function getTaskById(task_id) {
+  return await db("tasks").where({ task_id }).select("*");
+}
+
+async function addTask(task) {
+  const newTaskId = await db("tasks").insert(task);
+  const [newTask] = await getTaskById(newTaskId);
+  return {
+    task_id: newTask.task_id,
+    task_description: newTask.task_description,
+    task_notes: newTask.task_notes,
+    task_completed: Boolean(newTask.task_completed),
+    project_id: newTask.project_id,
+  };
+}
+
+module.exports = {
+  addTask,
+  getTasks,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,25 @@
 // build your `/api/tasks` router here
+const express = require("express");
+const { getTasks, addTask } = require("./model");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await getTasks();
+    res.status(200).json(tasks);
+  } catch (e) {
+    res.status(500).json({ message: "error getting tasks" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newTask = await addTask(req.body);
+    res.status(201).json(newTask);
+  } catch (e) {
+    res.status(500).json({ message: "error adding task" });
+  }
+});
+
+module.exports = router;
